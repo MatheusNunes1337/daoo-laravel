286d8f485af46156a8bb56430b6769dfe19c38f6
Matheus Nunes <matheusyagami147@gmail.com>
2022-03-05 16:10:14 -0300

    feat: create migration to create the group table

---
+++ database/migrations/2022_03_05_190541_create_grupos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGruposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grupos', function (Blueprint $table) {
+            $table->id();
+            $table->text('nome');
+            $table->text('descricao');
+            $table->text('disciplina');
+            $table->integer('max_membros');
+            $table->boolean('eh_publico')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grupos');
+    }
+}
