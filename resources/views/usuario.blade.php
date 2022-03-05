bfac2d04e0f74328afcdd35ca32f762a016d3c3a
Matheus Nunes <matheusyagami147@gmail.com>
2022-03-05 16:03:55 -0300

    feat: create template to list a user by id

---
+++ resources/views/usuario.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Produtos</title>
+</head>
+<body>
+    <h1>Usuario</h1>
+    @if ($usuario->count()>0)
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nome de usuário</th>
+                <th>email</th>
+                <th>tipo</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{$usuario->id}}</td>
+                <td>{{$usuario->username}}</td>
+                <td>{{$usuario->email}}</td>
+                <td>{{$usuario->tipo}}</td>
+            </tr>
+        </tbody>
+    </table>
+    @else
+    <p>Usuário não encontrado! </p>
+    @endif
+    <a href="/usuarios">voltar</a>
+</body>
+</html>
