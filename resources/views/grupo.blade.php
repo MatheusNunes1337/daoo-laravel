92eab053a01fa5766759436053fe6f58adc1103e
Matheus Nunes <matheusyagami147@gmail.com>
2022-03-05 16:22:35 -0300

    feat: create template to list a group by id

---
+++ resources/views/grupo.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grupo</title>
+</head>
+<body>
+    <h1>Grupo</h1>
+    @if ($grupo->count()>0)
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Descricao</th>
+                <th>disciplina</th>
+                <th>Nº máximo de membros</th>
+                <th>Público</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{$grupo->id}}</td>
+                <td>{{$grupo->nome}}</td>
+                <td>{{$grupo->descricao}}</td>
+                <td>{{$grupo->disciplina}}</td>
+                <td>{{$grupo->max_membros}}</td>
+                <td>{{($grupo->eh_publico)?'Sim':'Não'}}</td>
+            </tr>
+        </tbody>
+    </table>
+    @else
+    <p>Grupo não encontrado! </p>
+    @endif
+    <a href="/grupos">voltar</a>
+</body>
+</html>
