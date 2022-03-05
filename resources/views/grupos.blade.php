eda80168a25514ed5daf5cc74d4b2ff05891be12
Matheus Nunes <matheusyagami147@gmail.com>
2022-03-05 16:23:03 -0300

    feat: create template to list all groups

---
+++ resources/views/grupos.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Grupos</title>
+</head>
+<body>
+    <h1>Grupos</h1>
+    @if ($grupos->count()>0)
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Descricao</th>
+                <th>disciplina</th>
+                <th>Nº máximo de membros</th>
+                <th>Público</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($grupos as $grupo)
+            <tr>
+                <td><a href="/grupos/{{$grupo->id}}">{{$grupo->id}}</a></td>
+                <td>{{$grupo->nome}}</td>
+                <td>{{$grupo->descricao}}</td>
+                <td>{{$grupo->disciplina}}</td>
+                <td>{{$grupo->max_membros}}</td>
+                <td>{{($grupo->eh_publico)?'Sim':'Não'}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @else
+    <p>Grupos não encontrados! </p>
+    @endif
+</body>
+</html>
