a9ceb84312f533e27168f9108ec9569edc829e64
Matheus Nunes <matheusyagami147@gmail.com>
2022-03-05 15:48:52 -0300

    feat: create migration to create the users table

---
+++ database/migrations/2022_03_05_184234_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->text('username');
+            $table->text('senha');
+            $table->text('email');
+            $table->text('tipo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
