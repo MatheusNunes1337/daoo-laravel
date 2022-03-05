85b4185eb65b28b0f5c2e6ef8c1d0fff9c8ff8b1
Matheus Nunes <matheusyagami147@gmail.com>
2022-03-05 15:40:22 -0300

    feat: create show method of ProdutoController

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -7,11 +7,26 @@ use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
+    /**
+     * Update the specified user.
+     *
+     * @param  Request  $request
+     * @param  string  $id
+     * @return Response
+     */
+
+
     private $produto;
 
     public function index() {
         $this->produto = new Produto();
         //return response()->json($this->produto->all());
         return view('produtos', ['produtos'=>$this->produto->all()]);
+    }
+
+    public function show(Request $request, $id) {
+        $this->produto = new Produto();
+        //$this->id = $request->id;
+        return view('produto', ['produto'=>$this->produto->find($id)]);
     }
 }
