02d08e4dae517ba95e1c4772056d73024b7fcfaa
Matheus Nunes <matheusyagami147@gmail.com>
2022-03-26 13:56:40 -0300

    feat: create produtos endpoints inside routes/api folder

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\FornecedorController;
+use App\Http\Controllers\Api\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/produtos',[ProdutoController::class,'index']);
+Route::get('/produtos/{id}',[ProdutoController::class,'show']);
+Route::post('/produtos',[ProdutoController::class,'store']);
+
+Route::put('/produtos/{id}',[ProdutoController::class,'update']);
+Route::delete('/produtos/{id}',[ProdutoController::class,'delete']);
+
+Route::apiResource('fornecedores',FornecedorController::class)
+            ->parameters(["fornecedores"=>"fornecedor"]);
+
+Route::get('fornecedores/{fornecedor}/produtos',[FornecedorController::class,'listProdutos'])
+->name('fornecedores.produtos');
