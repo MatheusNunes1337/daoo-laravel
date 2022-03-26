542bd822044e79989caf728f793e1d87dc448af4
Matheus Nunes <matheusyagami147@gmail.com>
2022-03-26 13:53:42 -0300

    feat: add relationship to Produto Model

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'descricao',
+        'qtd_estoque',
+        'preco',
+        'importado',
+        'fornecedor_id'
+    ];
+
+    public function fornecedores()
+    {
+        return $this->belongsTo(Fornecedor::class);
+    }
+
 }
